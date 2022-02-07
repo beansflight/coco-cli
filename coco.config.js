aca6e8b1dffa8cf69832c0240ed86907e7f297ca
miyaliunian <354849262@qq.com>
2022-02-07 18:55:59 +0800

    release project

---
+++ coco.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "coco-template",
+  templateName: "营销活动",
+  author: "muwoo",
+  snapshot: "https://cdn.img.snapshot.png",
+  gitUrl: "beansflight/coco-template",
+  projectType: "0",
+};
